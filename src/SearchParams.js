1c7904d6c672d605afb2ea5efb433d593c1cf42d
Igor <mctubegui@gmail.com>
2019-06-29 22:16:26 -0300

    eslintconfig and more

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { ANIMALS } from '@frontendmasters/pet';
+import useDropdown from './useDropdown';
 
 const SearchParams = () => {
-  const location = 'Seattle, WA';
+  //A linha abaixo é um Hook. Todos os Hooks começam com use (Ex: useState)
+  //You can put a hook definition inside a if statement or a for loop, etc.
+  const [location, setLocation] = useState('Seattle, WA');
+  const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropdown, setAnimal] = useDropdown(
+    'Animal',
+    'dog',
+    ANIMALS
+  );
+  const [breed, BreedDropdown, setBreed] = useDropdown('Breed', '', breeds);
 
   return (
     <div className="search-params">
@@ -13,8 +24,44 @@ const SearchParams = () => {
             id="location"
             value={location}
             placeholder="Location"
+            onChange={e => setLocation(e.target.value)}
           />
         </label>
+        <AnimalDropdown />
+        <BreedDropdown />
+        {/* <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={e => setAnimal(e.target.value)}
+            onBlur={e => setAnimal(e.target.value)}
+          >
+            <option>All</option>
+            {ANIMALS.map(animal => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={e => setBreed(e.target.value)}
+            onBlur={e => setBreed(e.target.value)}
+            disabled={breeds.length === 0}
+          >
+            <option>All</option>
+            {breeds.map(breedString => (
+              <option key={breedString} value={breedString}>
+                {breedString}
+              </option>
+            ))}
+          </select>
+        </label> */}
         <button>Submit</button>
       </form>
     </div>
