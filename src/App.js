a6ad5151d11bd917da6c26b84759fc9ee211e0ff
Igor <mctubegui@gmail.com>
2019-06-29 21:18:58 -0300

    Setting up new plugins for eslint and jsx notation

--- src/App.js
+++ src/App.js
@@ -3,38 +3,49 @@ import { render } from 'react-dom';
 import Pet from './Pet';
 
 const App = () => {
-  return React.createElement(
-    'div',
-    {
-      id: 'something important'
-    },
-    [
-      React.createElement('h1', {}, 'Adopt Me!'),
-      React.createElement(Pet, {
-        name: 'Luna',
-        animal: 'Dog',
-        breed: 'Havanese'
-      }),
-      React.createElement(Pet, {
-        name: 'Pepper',
-        animal: 'Bird',
-        breed: 'Cockatiel'
-      }),
-      React.createElement(Pet, {
-        name: 'Doink',
-        animal: 'Cat',
-        breed: 'Stray'
-      }),
-      React.createElement(Pet, {
-        name: 'Gabriel',
-        animal: 'Cavalo',
-        breed: 'animal'
-      })
-    ]
+  // return React.createElement(
+  //   'div',
+  //   {
+  //     id: 'something important'
+  //   },
+  //   [
+  //     React.createElement('h1', {}, 'Adopt Me!'),
+  //     React.createElement(Pet, {
+  //       name: 'Luna',
+  //       animal: 'Dog',
+  //       breed: 'Havanese'
+  //     }),
+  //     React.createElement(Pet, {
+  //       name: 'Pepper',
+  //       animal: 'Bird',
+  //       breed: 'Cockatiel'
+  //     }),
+  //     React.createElement(Pet, {
+  //       name: 'Doink',
+  //       animal: 'Cat',
+  //       breed: 'Stray'
+  //     }),
+  //     React.createElement(Pet, {
+  //       name: 'Gabriel',
+  //       animal: 'Cavalo',
+  //       breed: 'animal'
+  //     })
+  //   ]
+  // );
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+      <Pet name="Doink" animal="Cat" breed="Stray" />
+    </div>
   );
 };
 
 //React.createElement(element, attributes, children -> can be a array);
 
 //Overwrites everything inside div id="root" element
-render(React.createElement(App), document.getElementById('root'));
+
+// render(React.createElement(App), document.getElementById('root'));
+
+render(<App />, document.getElementById('root'));
--- src/Pet.js
+++ src/Pet.js
@@ -1,11 +1,18 @@
 import React from 'react';
 //Both definitions works the same way
-export default function Pet(props) {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, props.name),
-    React.createElement('h2', {}, props.animal),
-    React.createElement('h2', {}, props.breed)
-  ]);
+export default function Pet({ name, animal, breed }) {
+  //   return React.createElement('div', {}, [
+  //     React.createElement('h1', {}, props.name),
+  //     React.createElement('h2', {}, props.animal),
+  //     React.createElement('h2', {}, props.breed)
+  //   ]);
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h2>{animal}</h2>
+      <h2>{breed}</h2>
+    </div>
+  );
 }
 
 //Destructure
